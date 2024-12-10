049c0133f5eaab583c30eca2e09ff03cb9dda7f2
Carlos De Jesus <dejecj@gmail.com>
2024-12-10 19:41:46 +0000

    Fix: add condition to respect monitor mode at the channel level

--- cron/src/workers/scheduler.ts
+++ cron/src/workers/scheduler.ts
@@ -1,16 +1,16 @@
 import Pocketbase from "pocketbase";
-import { ChannelVideo } from "../../../ui/types/channel";
-import { downloadVideo } from "@/queues";
+import { Channel, ChannelVideo } from "../../../ui/types/channel";
+import { downloadVideo, fetchVideoList } from "@/queues";
 import cron from 'node-cron';
 import { pinoInstance as pino } from "@/middlewares/pino-logger";
 const pb = new Pocketbase("http://localhost:8090");
 pb.collection("_superusers").authWithPassword("admin@ytarr.local", "admin_ytarr");
 
 export const videoMonitor =
-  cron.schedule('0 * * * *', async () => {
+  cron.schedule('30 * * * *', async () => {
     pino.info('Running video monitor task.')
     let pendingVideos = await pb.collection('channel_videos').getFullList<ChannelVideo>({
-      filter: 'monitored = true && status = "none"',
+      filter: 'monitored = true && status = "none" && channel.monitored != "none"',
       sort: "+published"
     });
 
